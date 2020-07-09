30e5691ad6de91a4e6f7b882d438e6076a66faa7
Pratiksha Bhangdiya <pratiksha@Amols-MacBook-Air.local>
2020-07-09 16:29:47 -0700

    showing vs-code github setup1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/Todoinput";
 import TodoList from "./components/TodoList";
 
+//showing vs-code github setup
+//showing vs-code github setup
 function App() {
   return (
     <div className="container">
