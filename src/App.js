43cdf82f2e0d255b70f938fa134d9280873d5877
Pratiksha Bhangdiya <pratiksha@Amols-MacBook-Air.local>
2020-07-09 16:15:07 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
-import uuid from "uuid";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/Todoinput";
 import TodoList from "./components/TodoList";
-import Todoinput from "./components/Todoinput";
 
 function App() {
   return (
     <div className="container">
       <div className="row">
-        <Todoinput></Todoinput>
+        <TodoInput></TodoInput>
         <TodoList></TodoList>
       </div>
     </div>
